9f4ea997ae8c7bb7f97758be1942bf6a7b0997a7
OleksandrRo <roghkovaa@gmail.com>
2022-08-05 09:53:31 +0200

    create first react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from './App'
+
+
+
+function Button (props) {
+
+function buttonOnClick(e) {
+ if (e.target.style.backgroundColor === 'green') {e.target.style.backgroundColor = 'yellow'}
+  
+else {e.target.style.backgroundColor = 'green'}
+}
+
+  return(
+  
+        <button onClick={buttonOnClick} className='button'> {props.name} {props.emoji}</button>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+root.render(
+    <>
+      <Button name = "Lion" emoji = "🦁"/>
+      <Button name = "Tiger" emoji = "🐯"/>
+      <Button name = "Dog" emoji = "🐩"/>
+      <Button name = "Wolf" emoji = "🐺"/>
+      <Button name = "Fox" emoji = "🦊"/>
+      <Button name = "Raccoon" emoji = "🦝"/>
+
+      <App placeholder="So" />
+      
+    </>
+)
+
+
+
+
+
+
+
