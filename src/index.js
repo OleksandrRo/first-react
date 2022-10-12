5f468c07184ecd49894c89b6718a632006945ac7
OleksandrRo <roghkovaa@gmail.com>
2022-10-12 16:12:12 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -2,48 +2,108 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function Button(props) {
 
-  let [normalState, setNormalState] = React.useState(true);
 
-  const counter = ()=>{
-      setNormalState (!normalState)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Button(props) {
+
+//   let [normalState, setNormalState] = React.useState(true);
+
+//   const counter = ()=>{
+//       setNormalState (!normalState)
 
       
 
-  }
+//   }
 
   
 
-  return (
-    <>
-      <div>
-       {normalState ? "hello":"buy"}
-      </div>
-      <button
-        style={{ backgroundColor: (normalState ? 'inherit' : 'red'), color: (normalState ? 'black' : 'white'), }}
-        onClick={counter}>
-        {props.emoji} {props.name} {props.emoji}
-      </button>
+//   return (
+//     <>
+//       <div>
+//        {normalState ? "hello":"buy"}
+//       </div>
+//       <button
+//         style={{ backgroundColor: (normalState ? 'inherit' : 'red') }}
+//         onClick={counter}>
+//         {props.emoji} {props.name} {props.emoji}
+//       </button>
       
-    </>
-  );
-}
+//     </>
+//   );
+// }
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-root.render(
-  <div>
-    <Button name="Lion" emoji="🦁" />
-    <Button name="Tiger" emoji="🐯" />
-    <Button name="Dog" emoji="🐩" />
-    <Button name="Wolf" emoji="🐺" />
-    <Button name="Fox" emoji="🦊" />
-    <Button name="Raccoon" emoji="🦝" />
-  </div>
-)
+// root.render(
+//   <div>
+//     <Button name="Lion" emoji="🦁" />
+//     <Button name="Tiger" emoji="🐯" />
+//     <Button name="Dog" emoji="🐩" />
+//     <Button name="Wolf" emoji="🐺" />
+//     <Button name="Fox" emoji="🦊" />
+//     <Button name="Raccoon" emoji="🦝" />
+//   </div>
+// )
 
 
 
